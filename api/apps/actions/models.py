7ccf644b9e32f5148b74b323e457e5f0bb93f406
Miguel Cabrera Victoria <miguelangelcabreravictoria@gmail.com>
2024-03-05 08:25:53 -0600

    feat(api): actions endpoints & models

--- api/apps/actions/models.py
+++ api/apps/actions/models.py
@@ -1,8 +1,28 @@
-from sqlalchemy import Column, Integer, String
-# from db import Base
+from pydantic import BaseModel
+from typing import List
 
 from pydantic import BaseModel
 
-class Action(BaseModel):
+class StartCallResponse(BaseModel):
+    call_id: str
+    message: str
+
+class EndCallResponse(BaseModel):
+    call_id: str
+    message: str
+
+class CreateContactResponse(BaseModel):
+    contact_id: str
+    contact_name: str
+    phone_number: str
+
+class CallDetailsResponse(BaseModel):
+    call_id: str
     name: str
-    description: str
+    number: str
+    call_subject: str
+
+class AgentAvailability(BaseModel):
+    agent_id: str
+    agent_name: str  
+    availability: str
