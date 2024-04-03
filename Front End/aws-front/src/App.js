66a9754279a9d7961e116b38e658e1d9a128527e
Juan Pablo Moreno <jpmoreno98@gmail.com>
2024-04-03 15:39:12 -0600

    Haciendo pruebas para el Nav bar

--- Front End/aws-front/src/App.js
+++ Front End/aws-front/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import  Home  from './pages/Home';
+import { NavBar } from './Componetes/nav-bar';
+import {BrowserRouter as Router, Route, Routes as Switch} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <NavBar/>
+      <Switch>
+        <Route path="/" exact Component={Home}>
+        </Route>
+      </Switch>
+      </Router>
     </div>
   );
 }
+
 
 export default App;
---
+++ Front End/aws-front/src/Componetes/nav-bar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "../Styles/nav-bar.css";
+import { Link } from "react-router-dom";
+
+
+
+export function NavBar() {
+    return (
+        <div className="navbar">
+            <div className="leftside">
+            </div>
+            <div className="rightside">
+            <Link to="/">Home</Link>
+            <Link to="/Dashboard">Dashboard</Link>
+            <Link to="/LogIn">Log In</Link>
+            <Link to="/Contact">Contact</Link>
+            </div>
+        </div>   
+    );
+}
