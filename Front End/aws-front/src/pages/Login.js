3a5084a137b80e6a3e9d28bdaa87b6ce995b3c8d
Juan Pablo Moreno <jpmoreno98@gmail.com>
2024-04-08 20:11:58 -0600

    Changin to look more like figma

--- Front End/aws-front/src/pages/Login.js
+++ Front End/aws-front/src/pages/Login.js
@@ -15,28 +15,20 @@ function LogIn() {
               <Card.Text className='text'>
                 Welcome Back!
               </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
             </Card.Body>
          </Card>
         </div>
          <div>
-          <Form className='rightForm'>
-            <img src={AmazonLogo} alt="Amazon Logo" />
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" style={{padding:"2px"}} />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control size='sm' type="password" placeholder="Password" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Sign In
-            </Button>
-          </Form>
+         <Card className='rightCard'>
+            <Card.Body> 
+              <Card.Text className='text'>
+                Welcome Back!
+              </Card.Text>
+              <Button variant="primary"><img src={AmazonLogo} alt="Amazon Logo" /></Button>
+              <br/>
+              <a href="http://">Forgot Password?</a>
+            </Card.Body>
+         </Card>
         </div>
       </div>
   );
