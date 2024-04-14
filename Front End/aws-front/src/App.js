a2508aa2c6bdf2ccc5c1c35e61dd32bc07f9ccbe
Fernanda <a01781983@tec.mx>
2024-04-14 17:51:29 -0600

    Admin.js page was created with 3 out of 4 screens, team, reports and calls. Also the chart.js files where added (Barchar.js and Piechar.js). Finally a .css file was added corresponding for all the components in admin.js.
    Co-authored-by: NatMayaB <nmaya0309@gmail.com>

--- Front End/aws-front/src/App.js
+++ Front End/aws-front/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import  Home  from './pages/Home';
+import  Dashboard  from './pages/admin';
 import { NavBar } from './Componetes/nav-bar';
 import {BrowserRouter as Router, Route, Routes as Switch} from "react-router-dom";
 
@@ -12,10 +13,13 @@ function App() {
         <Route path="/" exact Component={Home}>
         </Route>
       </Switch>
+      <Switch>
+        <Route path="/dashboard" exact Component={Dashboard}>
+        </Route>
+      </Switch>
       </Router>
     </div>
   );
 }
-
 
 export default App;
