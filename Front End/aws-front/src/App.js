4f4e28765739c5e7a25afb51162f7b7ea563e401
Juan Pablo Moreno <jpmoreno98@gmail.com>
2024-04-16 12:54:48 -0600

    Adding comments

--- Front End/aws-front/src/App.js
+++ Front End/aws-front/src/App.js
@@ -1,27 +1,20 @@
 import './App.css';
-import  Home  from './pages/Home';
-import Login from './pages/Login';
-import  Dashboard  from './pages/admin';
-import AgentCall from './pages/agentCall';
-import { NavBar } from './Componetes/nav-bar';
-import {BrowserRouter as Router, Route, Routes as Switch} from "react-router-dom";
+import Home from './pages/Home'; // Importa el componente de la página de inicio
+import Login from './pages/Login'; // Importa el componente de la página de inicio de sesión
+import Dashboard from './pages/admin'; // Importa el componente del dashboard del administrador
+import AgentCall from './pages/agentCall'; // Importa el componente del dashboard del agente
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"; // Importa componentes de React Router
 
 function App() {
   return (
     <div className="App">
-      <Router>
-      <Switch>
-        <Route path="/" exact Component={Home}/>
-      </Switch>
-      <Switch>
-        <Route path="/adminDashboard" exact Component={Dashboard}/>
-      </Switch>
-      <Switch>
-        <Route path="/agentDashboard" exact Component={AgentCall}/>
-      </Switch>
-      <Switch>
-        <Route path="/login" exact Component={Login}/>
-      </Switch>
+      <Router> {/* Componente Router para envolver la aplicación */}
+        <Switch> {/* Componente Switch para asegurar que solo se renderice una ruta a la vez */}
+          <Route path="/" exact component={Home} /> {/* Ruta para la página de inicio */}
+          <Route path="/adminDashboard" exact component={Dashboard} /> {/* Ruta para el dashboard del administrador */}
+          <Route path="/agentDashboard" exact component={AgentCall} /> {/* Ruta para el dashboard del agente */}
+          <Route path="/login" exact component={Login} /> {/* Ruta para la página de inicio de sesión */}
+        </Switch>
       </Router>
     </div>
   );
