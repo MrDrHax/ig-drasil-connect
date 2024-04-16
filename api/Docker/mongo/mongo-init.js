1e77e449bd34df577c4e339b0350c690ffdb78df
Miguel Cabrera Victoria <miguelangelcabreravictoria@gmail.com>
2024-04-15 18:37:53 -0600

    feat(mongo): DynamoDB Collections

---
+++ api/Docker/mongo/mongo-init.js
@@ -0,0 +1,59 @@
+db.createCollection("agent_notes", {
+    validator: {
+        $jsonSchema: {
+            bsonType: "object",
+            title: "Agent Notes",
+            required: ["_id", "date", "id_agent", "notes", "created_at", "updated_at"],
+            properties: {
+                _id: { bsonType: "objectId" },
+                date: { bsonType: "date" },
+                id_agent: { bsonType: "string" },
+                notes: {
+                    bsonType: "array",
+                    items: {
+                        bsonType: "object",
+                        required: ["message", "author", "timestamp"],
+                        properties: {
+                            message: { bsonType: "string" },
+                            author: { bsonType: "string" },
+                            timestamp: { bsonType: "date" }
+                        }
+                    }
+                },
+                created_at: { bsonType: "date" },
+                updated_at: { bsonType: "date" }
+            }
+        }
+    }
+});
+
+
+db.createCollection("supervisor_agent", {
+    validator: {
+      $jsonSchema: {
+        bsonType: "object",
+        title: "Supervisor and Agent conversations",
+        required: ["_id", "id_agent", "id_supervisor", "created_at", "updated_at", "messages"],
+        properties: {
+          _id: { bsonType: "objectId" },
+          id_agent: { bsonType: "string" },
+          id_supervisor: { bsonType: "string" },
+          created_at: { bsonType: "date" },
+          updated_at: { bsonType: "date" },
+          messages: {
+            bsonType: "array",
+            items: {
+              bsonType: "object",
+              required: ["message_id", "sender", "timestamp", "content"],
+              properties: {
+                message_id: { bsonType: "string" },
+                sender: { enum: ["supervisor", "agent"] },
+                timestamp: { bsonType: "date" },
+                content: { bsonType: "string" }
+              }
+            }
+          }
+        }
+      }
+    }
+  }); 
