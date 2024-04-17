537b36ac8bf11119ef11166368b19e3f2825f3ac
Juan Pablo Moreno <jpmoreno98@gmail.com>
2024-04-17 17:10:45 -0600

    Redoing front

---
+++ Front End/Connect Front/src/pages/dashboard/notifications.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import {
+  Typography,
+  Alert,
+  Card,
+  CardHeader,
+  CardBody,
+} from "@material-tailwind/react";
+import { InformationCircleIcon } from "@heroicons/react/24/outline";
+import { MessageCard } from "@/widgets/cards";
+
+export function Notifications() {
+  const [showAlerts, setShowAlerts] = React.useState({
+    blue: true,
+    green: true,
+    orange: true,
+    red: true,
+  });
+  const [showAlertsWithIcon, setShowAlertsWithIcon] = React.useState({
+    blue: true,
+    green: true,
+    orange: true,
+    red: true,
+  });
+  const alerts = ["gray", "green", "orange", "red"];
+
+  return (
+    <div className="mx-auto my-20 flex max-w-screen-lg flex-col gap-8">
+      <Card>
+        <CardHeader
+          color="transparent"
+          floated={false}
+          shadow={false}
+          className="m-0 p-4"
+        >
+          <Typography variant="h5" color="blue-gray">
+            Alerts
+          </Typography>
+        </CardHeader>
+        <CardBody className="flex flex-col gap-4 p-4">
+          {alerts.map((color) => (
+            <Alert
+              key={color}
+              open={showAlerts[color]}
+              color={color}
+              onClose={() => setShowAlerts((current) => ({ ...current, [color]: false }))}
+            >
+              A simple {color} alert with an <a href="#">example link</a>. Give
+              it a click if you like.
+            </Alert>
+          ))}
+        </CardBody>
+      </Card>
+      <Card>
+        <CardHeader
+          color="transparent"
+          floated={false}
+          shadow={false}
+          className="m-0 p-4"
+        >
+          <Typography variant="h5" color="blue-gray">
+            Alerts with Icon
+          </Typography>
+        </CardHeader>
+        <CardBody className="flex flex-col gap-4 p-4">
+          {alerts.map((color) => (
+            <Alert
+              key={color}
+              open={showAlertsWithIcon[color]}
+              color={color}
+              icon={
+                <InformationCircleIcon strokeWidth={2} className="h-6 w-6" />
+              }
+              onClose={() => setShowAlertsWithIcon((current) => ({
+                ...current,
+                [color]: false,
+              }))}
+            >
+              A simple {color} alert with an <a href="#">example link</a>. Give
+              it a click if you like.
+            </Alert>
+          ))}
+        </CardBody>
+      </Card>
+      <MessageCard>
+        <Typography variant="h5" color="blue-gray">
+          Message Card
+        </Typography>
+        <Typography variant="body" color="blue-gray">
+          This is a message card. It is used to display messages to the user.
+        </Typography>
+      </MessageCard>
+
+    </div>
+  );
+}
+
+export default Notifications;
