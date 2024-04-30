8e351495891a8dcd3f448483c3efb91260d97ccb
Alan Anthony Hernandez Perez <a01783347@tec.mx>
2024-04-30 16:15:09 -0600

    anvance al chat

--- Front End/Connect Front/src/pages/dashboard/agent.jsx
+++ Front End/Connect Front/src/pages/dashboard/agent.jsx
@@ -9,12 +9,6 @@ import {
   MenuHandler,
   MenuList,
   MenuItem,
-  Avatar,
-  Tooltip,
-  Progress,
-  Accordion,
-  AccordionBody,
-  AccordionHeader,
 } from "@material-tailwind/react";
 import {
   EllipsisVerticalIcon,
@@ -27,12 +21,11 @@ import {
   statisticsCardsDataAgent as statisticsCardsData,
   customerDataAgent,
   lexRecommendationData,
-  projectsTableData,
-  ordersOverviewData,
 } from "@/data";
 import { CheckCircleIcon, ClockIcon } from "@heroicons/react/24/solid";
 import {Notifications} from "../dashboard/notifications.jsx";
 import { getBgColor } from "@/context";
+import {TwitterChatboxTextarea , textArea } from "@/widgets/chat";
 
 
 
@@ -103,46 +96,49 @@ export function Agent() {
 
 {/*Aqui es sobre el sistema de alerta del home page */}
   <div className="mb-4 grid grid-cols-1 gap-6 xl:grid-cols-3">
-  <Card className={`overflow-hidden xl:col-span-2 border border-blue-gray-100 shadow-sm ${getBgColor("background-cards")}`}>
-    <CardHeader
-      floated={false}
-      shadow={false}
-      color="transparent"
-      className="m-0 flex items-center justify-between p-6"
-    >
-      <div>
-        <Typography variant="h6" color="blue-gray" className="mb-1">
-          Alerts
-        </Typography>
-        <Typography
-          variant="small"
-          className="flex items-center gap-1 font-normal text-blue-gray-600"
-        >
-          <CheckCircleIcon strokeWidth={3} className="h-4 w-4 text-blue-gray-200" />
-          <strong>10 alerts </strong>in this 30 minutes
-        </Typography>
-      </div>
-      <Menu placement="left-start">
-        <MenuHandler>
-          <IconButton size="sm" variant="text" color="blue-gray">
-            <EllipsisVerticalIcon
-              strokeWidth={3}
-              fill="currenColor"
-              className="h-6 w-6"
-            />
-          </IconButton>
-        </MenuHandler>
-        <MenuList>
-          <MenuItem>Action</MenuItem>
-          <MenuItem>Another Action</MenuItem>
-          <MenuItem>Something else here</MenuItem>
-        </MenuList>
-      </Menu>
-    </CardHeader>
-    <CardBody className="overflow-x-scroll px-0 pt-0 pb-2">
-      <Notifications />
-    </CardBody>
-  </Card>
+    <Card className={`overflow-hidden xl:col-span-2 border border-blue-gray-100 shadow-sm ${getBgColor("background-cards")}`}>
+      <CardHeader
+        floated={false}
+        shadow={false}
+        color="transparent"
+        className="m-0 flex items-center justify-between p-6"
+      >
+        <div>
+          <Typography variant="h6" color="blue-gray" className="mb-1">
+            Alerts
+          </Typography>
+          <Typography
+            variant="small"
+            className="flex items-center gap-1 font-normal text-blue-gray-600"
+          >
+            <CheckCircleIcon strokeWidth={3} className="h-4 w-4 text-blue-gray-200" />
+            <strong>10 alerts </strong>in this 30 minutes
+          </Typography>
+        </div>
+
+        <Menu placement="left-start">
+          <MenuHandler>
+            <IconButton size="sm" variant="text" color="blue-gray">
+              <EllipsisVerticalIcon
+                strokeWidth={3}
+                fill="currenColor"
+                className="h-6 w-6"
+              />
+            </IconButton>
+          </MenuHandler>
+          <MenuList>
+            <MenuItem>Action</MenuItem>
+            <MenuItem>Another Action</MenuItem>
+            <MenuItem>Something else here</MenuItem>
+          </MenuList>
+        </Menu>
+      </CardHeader>
+      <CardBody className="overflow-x-scroll px-0 pt-0 pb-2">
+        <Notifications />
+      </CardBody>
+    </Card>
+
+
   {/* Aquí esta para cambiar el Card de recomendaciones*/}
   <Card className={`border border-blue-gray-100 shadow-sm ${getBgColor("background-cards")}`}>
     <CardHeader
@@ -182,7 +178,30 @@ export function Agent() {
         openhandler={() => handleOpen(3)}/>
     </CardBody>
   </Card>
-  </div> 
+  </div>
+
+  {/* Aquí estael chat del agente*/}
+  <Card className={`overflow-hidden xl:col-span-2 border border-blue-gray-100 shadow-sm ${getBgColor("background-cards")}`}>
+      <CardHeader
+        floated={false}
+        shadow={false}
+        color="transparent"
+        className="m-0 flex items-center justify-between p-6"
+      >
+        <div>
+          <Typography variant="h6" color="blue-gray" className="mb-1">
+            Chat
+          </Typography>
+        </div>
+      </CardHeader>
+      <CardBody className="overflow-y-scroll px-0 pt-0 pb-2">
+        <Notifications />
+      </CardBody>
+      <CardBody className="p-4">
+        <TwitterChatboxTextarea />
+      </CardBody>
+    </Card>
+
   </div>
   );
   }
---
+++ Front End/Connect Front/src/widgets/chat/index.js
@@ -0,0 +1,3 @@
+
+
+export * from "@/widgets/chat/chat.jsx";
