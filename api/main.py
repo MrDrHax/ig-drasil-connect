6b3dbec7de2aada8e2521dfd3e085e3a55db8c82
MrDrHax <alexfh2001@gmail.com>
2024-05-02 00:43:06 -0600

    Add token checks and start of refresh code.

--- api/main.py
+++ api/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
+import fastapi_cdn_host
 from starlette.middleware.cors import CORSMiddleware
 import sys
+from AAA.requireToken import TokenRefreshMiddleware
 
 try:
     # Load environment variables
@@ -23,6 +25,12 @@ except ImportError:
     from .apps.extras.endpoints import router as extras_router
 
 app = FastAPI(debug=Config.DEBUG, title="Ig-drasil connect API", description="API for Ig-drasil connect dashboard", version="0.1.0", redoc_url="/docs", docs_url="/swagger")
+
+# change docs cdn host
+fastapi_cdn_host.patch_docs(app, docs_cdn_host='https://gcore.jsdelivr.net/npm')
+
+# Add middleware for token refresh
+app.add_middleware(TokenRefreshMiddleware)
 
 # Add CORS middleware
 app.add_middleware(
