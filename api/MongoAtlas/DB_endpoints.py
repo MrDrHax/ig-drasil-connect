b9ccc0a12d422fd2d47c5caf5930e98dde696ef7
MarioFriasPina <mario99fpina@gmail.com>
2024-05-21 11:17:44 -0600

    Chat communication in backend

--- api/MongoAtlas/DB_endpoints.py
+++ api/MongoAtlas/DB_endpoints.py
@@ -1,12 +1,22 @@
 import boto3
 from typing import List
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
 from config import Config
 from MongoAtlas.DB_connection import DataBase
+from . import models
+
+from typing import Annotated
+from AAA.requireToken import requireToken
+import AAA.userType as userType
+
+from datetime import datetime, timezone
+from base64 import b64encode, b64decode
+
+import logging
+logger = logging.getLogger(__name__)
 
 db_instance = DataBase.get_instance()
 db = db_instance.get_database()
-
 
 router = APIRouter(
     prefix="/MongoAtlas",
@@ -41,3 +51,47 @@ async def get_contact_lens():
     col.insert_one(response)
     return response
 
+#Chat endpoints
+
+@router.get('/get_chat_by_id', tags=["DB"])
+async def get_chat_by_id(token: Annotated[str, Depends(requireToken)], agent_id: str) -> List[models.ChatMessageBody]:
+    """
+    Returns all chats from a specific agent ID.
+    """
+    if not userType.isManager(token) and not userType.isAgent(token):
+        raise HTTPException(status_code=401, detail="Unauthorized. You must be a manager or an agent to access this resource.")
+    
+#    return db['chats'].find_one({"agent_id": agent_id})
+
+    chat = db['chats'].find_one({"agent_id": agent_id})
+
+    for index, message in enumerate(chat['messages']):
+        chat['messages'][index]['content'] = b64decode(message['content']).decode('utf-8').split(Config.BASE64AUTH)[0]
+
+    return chat['messages']
+
+@router.get('/get_chat', tags=["DB"])
+async def get_chat(token: Annotated[str, Depends(requireToken)]) -> List[models.ChatMessage]:
+    """
+    Returns a list of all chats.
+    """    
+
+    if not userType.isManager(token) and not userType.isAgent(token):
+        raise HTTPException(status_code=401, detail="Unauthorized. You must be a manager or an agent to access this resource.")
+    
+    return db['chats'].find()
+
+@router.post('/post_chat', tags=["DB"])
+async def post_chat(token: Annotated[str, Depends(requireToken)], message: str, agent_id: str, supervisor: bool) -> str:
+    """
+    Posts a chat message.
+    """
+    if not userType.isManager(token) and not userType.isAgent(token):
+        raise HTTPException(status_code=401, detail="Unauthorized. You must be a manager or an agent to access this resource.")
+    
+    salted_message = message + Config.BASE64AUTH + agent_id
+    encoded_message = b64encode(salted_message.encode('utf-8'))
+    #logger.info(b64decode(encoded_message).decode('utf-8'))
+    db['chats'].update_one({"agent_id": agent_id}, {"$push": {"messages": {"content": encoded_message, "supervisor_sender": supervisor, "timestamp": datetime.now(tz=timezone.utc)}}}, upsert=True)
+
+    return "success"
